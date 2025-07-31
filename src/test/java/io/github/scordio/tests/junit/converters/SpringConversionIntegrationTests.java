480bb56df6add59768b85bfc9890f28b92d6ff6f
Stefano Cordio <stefano.cordio@gmail.com>
2025-08-01 01:52:44 +0200

    Add `@SpringConversion` converter

---
+++ src/test/java/io/github/scordio/tests/junit/converters/SpringConversionIntegrationTests.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright © 2025 Stefano Cordio
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.scordio.tests.junit.converters;
+
+import io.github.scordio.junit.converters.SpringConversion;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static io.github.scordio.tests.junit.converters.JupiterEngineTestKit.executeTestsForClass;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class SpringConversionIntegrationTests {
+
+	@Test
+	void should_convert_supported_values() {
+		executeTestsForClass(SupportedValuesTestCase.class).testEvents()
+			.assertStatistics(stats -> stats.started(2).succeeded(2));
+	}
+
+	static class SupportedValuesTestCase {
+
+		@ParameterizedTest
+		@MethodSource("springArguments")
+		void test(@SpringConversion List<Integer> strings, List<Integer> expected) {
+			assertThat(strings).isEqualTo(expected);
+		}
+
+		static Stream<Arguments> springArguments() {
+			return Stream.of( //
+					arguments("123, 456", List.of(123, 456)), //
+					arguments(List.of("123", "456"), List.of(123, 456)));
+		}
+
+	}
+
+}
